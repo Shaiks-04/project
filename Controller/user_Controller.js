05c476155a80403f7d6ad40b8aabd11075c18e8c
unknown <shaikrahaman04@gmail.com>
2023-08-23 21:43:24 +0530

    initial commit

--- Controller/user_Controller.js
+++ Controller/user_Controller.js
@@ -1,6 +1,6 @@
 const user=require('../Model/userStructure')
 const saltround=10
-const bcrypt=require('bcrypt')
+const bcrypt=require('bcryptjs')
 const jwt=require('jsonwebtoken')
 const secretKey='shaik'
 
